3356ea998867c2605e83361cfafb53272365a802
Husam Hammad <85282854+husamhammad@users.noreply.github.com>
2024-07-01 19:59:45 +0300

    feat: Remove the try except from the function and use the frappe throw instead of it (#2)

--- branchy_playground/utils.py
+++ branchy_playground/utils.py
@@ -3,12 +3,10 @@ from frappe import _
 
 def get_employee_by_user(user):
     if not user:
-        raise ValueError("User ID must be provided")
-    try:
-        employee = frappe.get_value("Employee", {"user_id": user}, "name")
-        if not employee:
-            frappe.throw(f"error: Employee not found for the given user {user}")
+        frappe.throw(_("User ID must be provided"))
 
-        return employee
-    except Exception as e:
-        raise e
+    employee = frappe.get_value("Employee", {"user_id": user}, "name")
+    if not employee:
+        frappe.throw(_(f"Employee not found for the given user ID: {user}"))
+        
+    return employee
